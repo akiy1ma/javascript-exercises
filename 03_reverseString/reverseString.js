10276706b3a7a453bfd216f56eeeb6baa78a4d35
akiyama <iwanttob2.7lieve@gmail.com>
2024-11-08 14:40:36 +0300

    task 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('').reverse().join('')
+    /*const arr = Array.from(string);
+    arr.reverse();
+    let reversedString = arr.join('')*/
+    /*let reversedString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }*/
+    // return reversedString;
 };
 
 // Do not edit below this line
