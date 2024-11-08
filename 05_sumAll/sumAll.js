02372ef85efb3a128df510803fba86da8d21fe03
akiyama <iwanttob2.7lieve@gmail.com>
2024-11-08 22:16:05 +0300

    task 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
+    let sum = 0;
+    // the if statement below allows function to take arguments in any order
+    if (min >= max) [min, max] = [max, min];
+
+    for (let num = min; num <= max; num++) {
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
