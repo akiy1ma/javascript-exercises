c8b777ef44a790b5651f7a6fec7813aa0b908260
akiyama <iwanttob2.7lieve@gmail.com>
2024-11-08 14:10:23 +0300

    task 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let repeatedString = '';
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        repeatedString = repeatedString + string; 
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
