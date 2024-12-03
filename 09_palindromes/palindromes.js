1128fceacf18e38fba45b18291570deebd75f667
akiyama <iwanttob2.7lieve@gmail.com>
2024-12-04 02:28:10 +0300

    task 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    function isLetter(char) {
+        return /^[a-zA-Z1-9]$/.test(char);
+    };
 
+    str = str.toLowerCase();
+    str = str.split('')
+    str = str.filter(isLetter);
+    for (i = 0; i < str.length; i++) {
+        if (str[i] != str[str.length-1-i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
